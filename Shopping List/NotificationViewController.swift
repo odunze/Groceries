7638db840d295c778a64918fe8c3711fb78edd5b
Lotanna <sugabelly@gmail.com>
2018-10-26 12:43:19 -0400

    added my own custom images
    also set up storyboard and collectionview cell

---
+++ Shopping List/NotificationViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationViewController.swift
+//  Shopping List
+//
+//  Created by Lotanna Igwe-Odunze on 10/26/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
